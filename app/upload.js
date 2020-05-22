9d692e30a414c1b15101b77c970a5b52273f1f32
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-05-22 09:51:48 +0200

    update user info if user click to upload another file

--- app/upload.js
+++ app/upload.js
@@ -1,9 +1,6 @@
 new Vue({
     created() {
-        var that = this;
-        call_xhr('GET', '/user/info', null).then(function (info) {
-            that.info = info;
-        })
+        this.get_user_info()
     },
     data: {
         upload: {
@@ -69,6 +66,16 @@ new Vue({
             this.uploaded.file_name = file.name;
             this.uploaded.size = file.size;
         },
+        get_user_info() {
+            var that = this;
+            call_xhr('GET', '/user/info', null).then(function (info) {
+                that.info = info;
+            })
+        },
+        back_to_upload_choice() {
+            this.uploaded.get_url = undefined
+            this.get_user_info() // update it
+        }
     },
     filters: {
         formatBytes: formatBytes,
