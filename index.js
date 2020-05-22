e01d39da414f0b9d057c634dee6d77ec2ab5efb5
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-05-22 21:13:41 +0200

    differentiate get_session and require_session

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ const helpers = require('./api/helpers')
 
 const app = express()
 
-const require_session = session({
+const get_session = session({
     secret: conf.session.secret, resave: false, saveUninitialized: false, 
     cookie: { maxAge: helpers.minutes_to_ms(5) },
 })
+const require_session = (req, res, next) => {
+    if (!req.session.user) throw "need relog"
+    next()
+}
 
-app.use('/user', require_session)
+app.use('/user', get_session, require_session)
 app.put('/user/upload', api.handle_upload)
 app.post('/user/upload', api.handle_upload)
 app.get('/user/info', api.user_info)
@@ -24,7 +28,7 @@ app.get('/user/file/:id', api.user_file)
 
 app.get('/get', api.handle_download) 
   
-app.use([/^\/$/, '/manage.html', '/manage-file.html'], require_session, shib.ensure_connected)
+app.use([/^\/$/, '/manage.html', '/manage-file.html'], get_session, shib.ensure_connected)
 
 app.use("/node_modules", express.static(__dirname + '/node_modules'))
 app.use(express.static(__dirname + '/app'))
